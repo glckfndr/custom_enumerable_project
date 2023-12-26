9ce18945fd9112205b6971a5d7db3813d13059b5
glckfndr <obulan65@gmail.com>
2023-12-26 21:39:05 +0200

    feat: Add all my_methods

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,65 @@
 module Enumerable
-  # Your code goes here
+  def my_each_with_index
+    ind = 0
+    self.my_each do |el|
+      yield el, ind
+      ind += 1
+    end
+  end
+
+  def my_count
+    return size unless block_given?
+    counter = 0
+    self.my_each do |el|
+      counter += 1 if (yield el)
+    end
+    counter
+  end
+
+  def my_select
+    selection = []
+    self.my_each do |el|
+      selection << el if (yield el)
+    end
+    selection
+  end
+
+  def my_map
+    selection = []
+    self.my_each do |el|
+      selection << (yield el)
+    end
+    selection
+  end
+
+  def my_inject(initial_val)
+    sum = initial_val
+    self.my_each do |el|
+      sum = (yield sum, el)
+    end
+    sum
+  end
+
+  def my_none?
+    self.my_each do |el|
+      return false  if (yield el)
+    end
+    true
+  end
+
+  def my_all?
+    self.my_each do |el|
+      return false  unless (yield el)
+    end
+    true
+  end
+
+  def my_any?
+    self.my_each do |el|
+      return true  if (yield el)
+    end
+    false
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +68,12 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    c = 0
+    until c == size
+      yield self[c]
+      c += 1
+    end
+    self
+  end
 end
